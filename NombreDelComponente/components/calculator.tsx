b0b2d55db4baf0a32424d9cce38bc85d0d649138
junsegui <juansegui06@gmial.com>
2023-05-15 13:02:49 -0300

    first commit

---
+++ NombreDelComponente/components/calculator.tsx
@@ -0,0 +1,47 @@
+import { TextField } from "@fluentui/react/lib/TextField";
+import { Label } from "@fluentui/react/lib/Label";
+import * as React from "react";
+import { Stack } from "@fluentui/react/lib/Stack";
+import { ServiceProvider } from "pcf-react";
+
+import { observer } from "mobx-react";
+import { IconButton } from "@fluentui/react/lib/Button";
+import { TaxCalculatorViewModel } from "../viewmodels/TaxCalculatorViewModels";
+import { NeutralColors } from "@fluentui/react";
+import { SharedColors } from '@fluentui/theme';
+
+export interface CalculatorComponentProps {
+  serviceProvider: ServiceProvider;
+}
+export class CalculatorComponent extends React.Component<CalculatorComponentProps> {
+  render(): JSX.Element {
+    const vm = this.props.serviceProvider.get("TaxCalculatorViewModel") as TaxCalculatorViewModel;
+    const { calculateMode, amount, tax, valueAfterTax } = vm;
+    return (
+      <Stack horizontal>
+        {calculateMode && (
+          <>
+            <TextField label="Amount" type="number" value={amount.toString()} onChange={vm.onChangeAmount} />
+            <TextField label="Tax %" type="number" value={tax.toString()} onChange={vm.onChangeTax} />
+            <TextField
+              label="Amount After Tax"
+              value={valueAfterTax.toString()}
+              type="number"
+              onChange={vm.onChangeValueAfterTax}
+            />
+            <IconButton iconProps={{ iconName: "Accept" }} onClick={vm.onAccept} style={{ color: SharedColors.magenta20 }} />
+            <IconButton iconProps={{ iconName: "Cancel" }} onClick={vm.onCancel} />
+          </>
+        )}
+        {!calculateMode && (
+          <>
+            <Label>{valueAfterTax}</Label>
+            <IconButton iconProps={{ iconName: "Calculator" }} onClick={vm.onCalculate}  />
+          </>
+        )}
+      </Stack>
+    );
+  }
+}
+
+observer(CalculatorComponent);
